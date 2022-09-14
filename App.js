ec4ac81c6856b9768edda08c5e8b728ee3240d9c
nhlamuloj <nmahlabana@gmail.com>
2022-09-14 16:13:56 +0200

    updated files

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,Button } from 'react-native';
+import { Audio } from 'expo-av';
 
 
 export default function App() {
@@ -10,12 +11,50 @@ export default function App() {
   async function startRacoding(){
     try{
       const permission = await Audio.requestPermissionAsync();
+
+      if(permission.status ==="granted"){
+        await Audio.setAudioModeAsync({
+          allowsRecordingIOS:true,
+          playsInSilentModeIOS:true,
+        });
+
+        const {recording} = await Audio.Recording.createAsync(
+          Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+        );
+
+        setRecording(recording);
+      }else{
+        setMessage("Please grant permission to app access Microphone")
+      }
+    } catch(err){
+      console.error('Failed to start recording',err);
     }
   }
 
-  async function stopRecording(){
+  async function stopRecording() {
+    setRecording(undefined);
+    await recording.StopAndUnloadAsync ();
 
+    let updatedRecordings =[... recording];
+    const {sound, status} =await recording.createNewLoadedSoundAsync();
+    updatedRecordings.push({
+      sound:sound,
+      duration:getDurationFormatted(status.durationMillis),
+      file:recording.getURI()
+    });
+
+    setRecordings(updatedRecordings);
   }
+
+  function getDurationFormatted(millis){
+    const minutes =millis/100/60
+    const minutesDisplay = Math.floor()
+    const seconds = Math.round ((minutes - minutesDisplay)*60);
+    const secondsDisplay = seconds < 10 ? `0${seconds}`: seconds;
+    return `${minutesDisplay}: ${secondsDisplay}`;
+  }
+
+  function getr
 
 
 
